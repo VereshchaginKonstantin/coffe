5362770dcdfc82d6b4df93405bf3a50008848e97
okarien <okarien@mail.ru>
2018-01-26 16:13:16 +0300

    add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SlideMenuModule } from 'cuppa-ng2-slidemenu/cuppa-ng2-slidemenu';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    SlideMenuModule,
     BrowserModule
   ],
   providers: [],
