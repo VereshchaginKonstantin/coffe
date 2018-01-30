abe0dfd4004bb74e83028d2705eca245ea3b661c
okarien <okarien@mail.ru>
2018-01-30 16:59:04 +0300

    fix CanActivateViaAuthGuard

--- src/app/can-activate-via-auth.guard.ts
+++ src/app/can-activate-via-auth.guard.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AuthenticationService } from './common/authentication.service'
 
 @Injectable()
 export class CanActivateViaAuthGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthenticationService) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (localStorage.getItem('currentUser')) {
+    if (this.authService.isLoggedIn()) {
       // logged in so return true
       return true;
-    } 
+    }
     // not logged in so redirect to login page
     this.router.navigate(['/login']);
     return false;
