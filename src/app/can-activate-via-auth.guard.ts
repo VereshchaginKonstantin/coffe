a33925c31b100b0bf8ed4c57c8e2aa3f004ddc90
okarien <okarien@mail.ru>
2018-01-30 17:06:02 +0300

    fd

--- src/app/can-activate-via-auth.guard.ts
+++ src/app/can-activate-via-auth.guard.ts
@@ -11,9 +11,11 @@ export class CanActivateViaAuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.authService.isLoggedIn()) {
+      console.log('isLoggedIn true');
       // logged in so return true
       return true;
     }
+    console.log('isLoggedIn false');
     // not logged in so redirect to login page
     this.router.navigate(['/login']);
     return false;
